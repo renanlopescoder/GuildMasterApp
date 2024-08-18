f68f4011949f8a4815e63ada2f0b3ef54f358c7b
Kennan Gauthier <KDGauthier@student.fullsail.edu>
2024-08-17 21:42:25 -0400

    Fixed the navigation logic for all fragments for both navigation menus.

--- app/src/main/java/com/ai/guildmasterapp/MainActivity.kt
+++ app/src/main/java/com/ai/guildmasterapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout = findViewById(R.id.drawer_layout) // Initializes drawerLayout
 
-       val bottomNavView: BottomNavigationView = binding.navView // Navigation for bottom menu items
+       val bottomNavView: BottomNavigationView = binding.navViewBottomMenu // Navigation for bottom menu items
 
         val navView: NavigationView = binding.navViewDrawerMenu // Navigation for drawer menu items
 
@@ -39,30 +39,105 @@ class MainActivity : AppCompatActivity() {
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,
                 R.id.navigation_messages, R.id.navigation_menu, R.id.navigation_news, R.id.navigation_pvp_history,
-                R.id.navigation_guild, R.id.navigation_community, R.id.navigation_crafting, R.id.navigation_my_profile
+                R.id.navigation_guild, R.id.navigation_community, R.id.navigation_crafting, R.id.navigation_my_profile,
+                R.id.navigation_about
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         bottomNavView.setupWithNavController(navController)
 
-        // Handle bottom nav item clicks
+        // Handles bottom menu item clicks, navigates to the appropriate page
         bottomNavView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_menu -> {
                     drawerLayout.openDrawer(GravityCompat.START)  // Opens side menu
                     true
                 }
-                else -> {
-                    NavigationUI.onNavDestinationSelected(item, navController)
+                R.id.navigation_dashboard -> {
+                    navController.navigate(R.id.navigation_dashboard)
                     true
                 }
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.navigation_home)
+                    true
+                }
+                R.id.navigation_notifications -> {
+                    navController.navigate(R.id.navigation_notifications)
+                    true
+                }
+                R.id.navigation_messages -> {
+                    navController.navigate(R.id.navigation_messages)
+                    true
+                }
+                else -> false
+            }
+
+        }
+
+        // When a drawer menu item is selected, it navigates to the appropriate page and closes the menu
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_dashboard -> {
+                    navController.navigate(R.id.navigation_dashboard)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_messages -> {
+                    navController.navigate(R.id.navigation_messages)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_news -> {
+                    navController.navigate(R.id.navigation_news)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_pvp_history -> {
+                    navController.navigate(R.id.navigation_pvp_history)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_pvp_history -> {
+                    navController.navigate(R.id.navigation_pvp_history)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_friends -> {
+                    navController.navigate(R.id.navigation_friends)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_guild -> {
+                    navController.navigate(R.id.navigation_guild)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_community -> {
+                    navController.navigate(R.id.navigation_community)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_crafting -> {
+                    navController.navigate(R.id.navigation_crafting)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.navigation_my_profile -> {
+                    navController.navigate(R.id.navigation_my_profile)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                else -> false
             }
         }
 
+        // When the about button is clicked
         navView.findViewById<Button>(R.id.button_about).setOnClickListener {
 
-            // Implement code to handle about button click
+            navController.navigate(R.id.navigation_about) // Navigates to about page.
+            drawerLayout.closeDrawer(GravityCompat.START) // Closes side menu
+
         }
 
         // When the log-out button is clicked
