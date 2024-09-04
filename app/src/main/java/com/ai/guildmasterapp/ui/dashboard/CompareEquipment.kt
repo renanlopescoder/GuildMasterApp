b5c19231936cbb4f8725539b8a348563069d2949
Jdad5150 <jjvlittle@gmail.com>
2024-09-03 23:38:41 -0400

    Able to fetch all equipment data, Update the icons to match the equipment, and update the stats based on the equipment. Working on a Splash Screen to pop-up to hide the loading.

--- app/src/main/java/com/ai/guildmasterapp/ui/dashboard/CompareEquipment.kt
+++ app/src/main/java/com/ai/guildmasterapp/ui/dashboard/CompareEquipment.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.ai.guildmasterapp.ui.dashboard
 
 import android.annotation.SuppressLint
@@ -20,6 +22,7 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.internal.platform.android.AndroidLogHandler.setLevel
 import java.net.SocketTimeoutException
 import java.util.concurrent.TimeUnit
 
@@ -65,6 +68,21 @@ data class Attribute(
     val modifier: Int
 )
 
+@Serializable
+data class CharacterAttributes(
+    var defense: Int = 0,
+    var power: Int = 0,
+    var toughness: Int = 0,
+    var precision: Int = 0,
+    var vitality: Int = 0,
+    var concentration: Int = 0,
+    var conditionDamage: Int = 0,
+    var expertise: Int = 0,
+    var ferocity: Int = 0,
+    var healingPower: Int = 0
+)
+
+
 
 
 class CompareEquipment : AppCompatActivity() {
@@ -76,6 +94,7 @@ class CompareEquipment : AppCompatActivity() {
     private lateinit var legsImage: ImageView
     private lateinit var feetImage: ImageView
     // Text Views
+    private lateinit var defenseAttribute: TextView
     private lateinit var powerAttribute: TextView
     private lateinit var toughnessAttribute: TextView
     private lateinit var precisionAttribute: TextView
@@ -94,6 +113,7 @@ class CompareEquipment : AppCompatActivity() {
         .build()
 
     val itemList = mutableListOf<Item>()
+    var baseAttributes = CharacterAttributes()
 
 
     @SuppressLint("MissingInflatedId")
@@ -114,6 +134,7 @@ class CompareEquipment : AppCompatActivity() {
         legsImage = findViewById(R.id.equipment_legs)
         feetImage = findViewById(R.id.equipment_feet)
 
+        defenseAttribute = findViewById(R.id.defense)
         powerAttribute = findViewById(R.id.power_attr)
         toughnessAttribute = findViewById(R.id.toughness_attr)
         precisionAttribute = findViewById(R.id.precision_attr)
@@ -123,13 +144,9 @@ class CompareEquipment : AppCompatActivity() {
         expertiseAttribute = findViewById(R.id.expertise_attr)
         ferocityAttribute = findViewById(R.id.ferocity_attr)
         healingPowerAttribute = findViewById(R.id.healing_power_attr)
-    ////////////////////////////////////////////////////////////////
-    //------------------DELETE AFTER TESTING----------------------//
-    ////////////////////////////////////////////////////////////////
 
 
-        val player = GlobalState.characterDetail
-
+//        This is a list of strings that is used to sort any equipment the character has DO NOT CHANGE
         val armorList = listOf(
             "Helm",
             "Shoulders",
@@ -139,53 +156,117 @@ class CompareEquipment : AppCompatActivity() {
             "Leggings"
         )
 
-        val playerEquipment = player?.equipment
-
+//      Using Coroutines to divide the logic and the UI
         CoroutineScope(Dispatchers.IO).launch {
-            for (item in playerEquipment!!) {
-                if (item.slot in armorList)
+            // Loop through all the items in the characters equipment and if the item.slot matches a string from the armorList, fecth the items details and add to itemList
+            for (item in GlobalState.characterDetail?.equipment!!) {
+                if (item.slot in armorList) {
                     fetchAndAddItem(item.id)
+                }
             }
 
-            runOnUiThread {
-                itemList.forEach{item ->
 
-                    when (item.details.type) {
-                        "Helm" -> {
-                            loadImageWithPicasso(helmImage,item.icon)
-                        }
-                        "Shoulders" -> {
-                            loadImageWithPicasso(shoulderImage,item.icon)
-                        }
-                        "Coat" -> {
-                            loadImageWithPicasso(chestImage,item.icon)
-                        }
-                        "Boots" -> {
-                            loadImageWithPicasso(feetImage,item.icon)
-                        }
-                        "Gloves" -> {
-                            loadImageWithPicasso(handsImage,item.icon)
-                        }
-                        "Leggings" -> {
-                            loadImageWithPicasso(legsImage,item.icon)
-                        }
+//      Calculate the base attributes based on the characters level and store them in baseAttributes
+            var baseAttributes = GlobalState?.characterDetail?.level?.let { calculateBaseAttributes(it) }
+
+
+            var characterAttributes = CharacterAttributes()
+
+            for (item in itemList) {
+                when (item.details.type) {
+                    "Helm" -> {
+                        val helmAttributes: CharacterAttributes = calculateInfixUpgrades(item)
+                        addAttributes(characterAttributes, helmAttributes)
+                    }
+
+                    "Shoulders" -> {
+                        val shouldersAttributes: CharacterAttributes = calculateInfixUpgrades(item)
+                        addAttributes(characterAttributes, shouldersAttributes)
+                    }
+
+                    "Coat" -> {
+                        val coatAttributes: CharacterAttributes = calculateInfixUpgrades(item)
+                        addAttributes(characterAttributes, coatAttributes)
+                    }
+
+                    "Boots" -> {
+                        val bootsAttributes: CharacterAttributes = calculateInfixUpgrades(item)
+                        addAttributes(characterAttributes, bootsAttributes)
+                    }
+
+                    "Gloves" -> {
+                        val glovesAttributes: CharacterAttributes = calculateInfixUpgrades(item)
+                        addAttributes(characterAttributes, glovesAttributes)
+                    }
+
+                    "Leggings" -> {
+                        val legsAttributes: CharacterAttributes = calculateInfixUpgrades(item)
+                        addAttributes(characterAttributes, legsAttributes)
                     }
                 }
             }
+
+            if (baseAttributes != null) {
+                addAttributes(characterAttributes, baseAttributes)
+            }
+
+//      Calculate the defense by adding all the equipments defense values together, and add toughness from baseAttributes
+            if (characterAttributes != null) {
+                characterAttributes.defense = (calculateInitialDefense() + baseAttributes?.toughness!!)
+            }
+
+            runOnUiThread {
+
+//              Loop through itemList and match the item to the Equipment slot, update image using Picasso
+                itemList.forEach { item ->
+                    when (item.details.type) {
+                        "Helm" -> loadImageWithPicasso(helmImage, item.icon)
+                        "Shoulders" -> loadImageWithPicasso(shoulderImage, item.icon)
+                        "Coat" -> loadImageWithPicasso(chestImage, item.icon)
+                        "Boots" -> loadImageWithPicasso(feetImage, item.icon)
+                        "Gloves" -> loadImageWithPicasso(handsImage, item.icon)
+                        "Leggings" -> loadImageWithPicasso(legsImage, item.icon)
+                    }
+                }
+
+//              Set all attributes from the characterAttributes
+                setDefense(characterAttributes.defense)
+                setPower(characterAttributes.power)
+                setToughness(characterAttributes.toughness)
+                setPrecision(characterAttributes.precision)
+                setVitality(characterAttributes.vitality)
+                setConcentration(characterAttributes.concentration)
+                setConditionDamage(characterAttributes.conditionDamage)
+                setExpertise(characterAttributes.expertise)
+                setFerocity(characterAttributes.ferocity)
+                setHealingPower(characterAttributes.healingPower)
+
+            }
+
         }
 
 
-
-
-    /////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////
-
-
-
     }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private fun returnToMain() {
         finish()
+    }
+
+    private fun calculateInitialDefense(): Int{
+        var totalEquipmentDefense: Int = 0
+
+        for (item in itemList) {
+            totalEquipmentDefense += item.details.defense
+        }
+
+        return totalEquipmentDefense
+    }
+
+    private fun setDefense(newDefense: Int){
+        val defenseString = getString(R.string.defense_attr) + newDefense.toString()
+        defenseAttribute.text = defenseString
     }
 
     private fun setPower(newPower: Int){
@@ -272,8 +353,84 @@ class CompareEquipment : AppCompatActivity() {
     }
 
     private fun loadImageWithPicasso(imageView: ImageView, url: String) {
-        Log.d("GLIDE", "$url")
+        Log.d("Picasso", "$url")
         Picasso.get().load(url).into(imageView)
     }
+
+    private fun calculateBaseAttributes(level: Int): CharacterAttributes{
+
+        val baseAttributes = CharacterAttributes()
+
+        //value that will be returned, starts at 37
+        var totalValue = 37
+
+        //value that will store the scalar as it iterates through the scalar values based on level.
+        var scalar: Int
+
+        //loop through the level and stop at the characters current level.
+        for (lvl in 2..level) {
+            scalar = when (lvl) {
+                in 2..10 -> 7
+                in 11..20 -> 10
+                in 21..24 -> 14
+                25, 26 -> 15
+                in 27..30 -> 16
+                in 31..40 -> 20
+                in 41..44 -> 24
+                45, 46 -> 25
+                in 47..50 -> 26
+                in 51..60 -> 30
+                in 61..64 -> 34
+                65, 66 -> 35
+                in 67..70 -> 36
+                in 71..74 -> 44
+                75, 76 -> 45
+                in 77..80 -> 46
+                else -> 0 // Default case, if level is out of range
+            }
+            totalValue += scalar
+        }
+
+        baseAttributes.power = totalValue
+        baseAttributes.toughness = totalValue
+        baseAttributes.precision = totalValue
+        baseAttributes.vitality = totalValue
+
+        return baseAttributes
+    }
+
+    private fun calculateInfixUpgrades(item: Item): CharacterAttributes{
+
+        val equipmentAttribute = CharacterAttributes()
+
+        item.details.infix_upgrade?.attributes?.forEach{ attributeModifier ->
+            when (attributeModifier.attribute) {
+                "Precision" -> equipmentAttribute.precision += attributeModifier.modifier
+                "Toughness" -> equipmentAttribute.toughness += attributeModifier.modifier
+                "Vitality" -> equipmentAttribute.vitality += attributeModifier.modifier
+                "Ferocity" -> equipmentAttribute.ferocity += attributeModifier.modifier
+                "ConditionDamage" -> equipmentAttribute.conditionDamage += attributeModifier.modifier
+                "Expertise" -> equipmentAttribute.expertise += attributeModifier.modifier
+                "Concentration" -> equipmentAttribute.concentration += attributeModifier.modifier
+                "HealingPower" -> equipmentAttribute.healingPower += attributeModifier.modifier
+                "Power" -> equipmentAttribute.power += attributeModifier.modifier
+            }
+        }
+        return equipmentAttribute
+    }
+
+    private fun addAttributes(attr1: CharacterAttributes, attr2: CharacterAttributes ){
+        attr1.defense += attr2.defense
+        attr1.power += attr2.power
+        attr1.toughness += attr2.toughness
+        attr1.vitality += attr2.vitality
+        attr1.concentration += attr2.concentration
+        attr1.healingPower += attr2.healingPower
+        attr1.precision += attr2.precision
+        attr1.conditionDamage += attr2.conditionDamage
+        attr1.expertise += attr2.expertise
+        attr1.ferocity += attr2.ferocity
+    }
+
 
 }
